13e273e45a150769a8bd613990b266b08c4da593
Saanyak <80775808+Saanyak@users.noreply.github.com>
2022-06-02 14:55:32 -0700

    Add files via upload

---
+++ notebook.js
@@ -0,0 +1,44 @@
+status = "";
+notebook_image = "";
+objects = [];
+
+function preload() {
+    notebook_image = loadImage("notebook.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(350,550);
+    canvas.center();
+    object_detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    status = true;
+    object_detector.Detect(notebook_image,gotResults);
+}
+
+function gotResults(results,error) {
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw() {
+    image(notebook_image,0,0,350,550);
+    if(status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+
+            fill("#ba53ed");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x, objects[i].y);
+            noFill();
+            stroke("#ba53ed");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
